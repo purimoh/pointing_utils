13cc5cc1de7cd776671a875b0c48b28d3947bd31
jgori <juliengori@gmail.com>
2023-05-26 15:29:27 +0200

    Bump version: 0.0.1 → 0.0.2-dev0

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ copyright = "2023, Julien Gori"
 author = "Julien Gori"
 
 # The full version, including alpha/beta/rc tags
-release = "0.0.1"
+release = "0.0.2-dev0"
 
 
 # -- General configuration ---------------------------------------------------
